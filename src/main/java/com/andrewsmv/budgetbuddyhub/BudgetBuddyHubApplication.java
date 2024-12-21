989708780c9016772050bac73e8938e480502962
AndrewSamoilov <andrew.smv94@gmail.com>
2024-12-21 23:58:08 +0200

    initial commit

---
+++ src/main/java/com/andrewsmv/budgetbuddyhub/BudgetBuddyHubApplication.java
@@ -0,0 +1,13 @@
+package com.andrewsmv.budgetbuddyhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetBuddyHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetBuddyHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andrewsmv/budgetbuddyhub/BudgetBuddyHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrewsmv.budgetbuddyhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBuddyHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
