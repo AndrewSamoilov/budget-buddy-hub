a4013b1c8d86890eeb3554c422b35a6a42bc3bac
AndrewSamoilov <andrew.smv94@gmail.com>
2024-12-22 00:11:17 +0200

    initial commit

---
+++ src/main/java/com/andrewsmv/budgetbuddyhub/domain/dto/LocalizationDto.java
@@ -0,0 +1,14 @@
+package com.andrewsmv.budgetbuddyhub.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LocalizationDto {
+
+    private String ua;
+    private String en;
+}
--- src/test/java/com/andrewsmv/budgetbuddyhub/BudgetBuddyHubApplicationTests.java
+++ src/test/java/com/andrewsmv/budgetbuddyhub/BudgetBuddyHubApplicationTests.java
@@ -1,9 +1,10 @@
 package com.andrewsmv.budgetbuddyhub;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@WebMvcTest
 class BudgetBuddyHubApplicationTests {
 
     @Test
